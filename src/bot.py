46fc9f57c849896e93f3217323199892bda0a323
perryliao <perryliaohc@gmail.com>
2019-01-31 17:33:15 -0800

    hooked up echo channel to a specified channel

--- src/bot.py
+++ src/bot.py
@@ -7,11 +7,18 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    if message.author is not client.user and validation.validateMention(message.content):
+    me = await client.get_user_info('92708688606793728')
+    print(message.author.name + ": " + message.content)
+    print(message.channel)
+    if message.author != client.user and message.author == me and message.channel.id == '540608326564839439': # and validation.validateMention(message.content):
+        if message.content == "!m close":
+            client.close()
+
         for usr in message.mentions:
             print (usr.mention)
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+        # msg = 'Hello {0.author.mention}'.format(message)
+
+        await client.send_message(client.get_channel('534191629762822144'), message.content)
     else:
         return
 
