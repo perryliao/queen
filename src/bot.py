9b3987abbe352d6f39a2f968472aa565b0ecd108
Perry Liao <perryliaohc@gmail.com>
2020-02-18 09:39:10 -0800

    remove useless code

--- src/bot.py
+++ src/bot.py
@@ -6,29 +6,18 @@ TOKEN = os.environ.get('QUEEN_TOKEN')
 client = discord.Client()
 
 reporting = client.get_channel('534191629762822144')
-test_channel = client.get_channel('540608326564839439')
-
+get_channel_id = '540608326564839439'
 arisa = client.get_server('539867992847024149')
+
 @client.event
 async def on_message(message):
     me = await client.get_user_info('92708688606793728')
-    dyxie = await client.get_user_info('225528966734151680')
-    rink = await client.get_user_info('92742418792714240')
 
-    if message.channel.id != '540608326564839439':
+    if message.channel.id != get_channel_id:
         print("[" + message.channel.name + "] " + message.author.name + ": " + message.content)
 
-    if message.author != client.user and message.author == me and message.channel == client.get_channel('540608326564839439'): # and validation.validateMention(message.content):
-        if message.content == "!m close":
-            client.logout()
-            print("didn't close...")
-        for usr in message.mentions:
-            print (usr.mention)
-        # msg = 'Hello {0.author.mention}'.format(message)
-
-        for role in message.server.roles:
-            await client.send_message(message.channel, role.id + role.name)
-        # await client.send_message(rink, message.content)
+    if message.author != client.user and message.author == me and message.channel == get_channel_id: # and validation.validateMention(message.content):
+        await client.send_message(reporting, message.content)
     else:
         return
 
